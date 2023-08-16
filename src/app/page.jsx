10cc83f653c8887d1f93c485748596943bcedbb2
acrisanto <alanvcrisanto@gmail.com>
2023-08-16 16:59:02 -0500

    add font styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,35 @@
-import Image from 'next/image'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className="">
-      <h1>Hola Aria desde main page</h1>
-      <div id='servicios'>
-        <h2>Productos</h2>
-      </div>
-    </main>
-  )
+	return (
+		<main className="p-8">
+			<div className={styles.background}>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div className="hero-section flex items-center justify-between">
+				<div className="title">
+					<h1 className="text-7xl font-bold">
+						<span className="text-purple-600">Explora</span> Tu Potencial con{" "}
+						<span className="text-purple-600">Aria Studio</span>
+					</h1>
+				</div>
+				<div className="image">
+					<Image src="/girl01.png" alt="girl with virtual glasses" width={700} height={400} />
+				</div>
+			</div>
+			<h1>Hola Aria desde main page</h1>
+			<div id="servicios">
+				<h2>Productos</h2>
+			</div>
+		</main>
+	);
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,42 +1,42 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./navbar.module.css"
+import styles from "./navbar.module.css";
 
 const links = [
-  {
-    id: 1,
-    title: "Home",
-    url: "#",
-  },
-  {
-    id: 2,
-    title: "Servicios",
-    url: "#servicios",
-  },
-  {
-    id: 3,
-    title: "Contacto",
-    url: "#contacto",
-  },
-
+	{
+		id: 1,
+		title: "Home",
+		url: "#",
+	},
+	{
+		id: 2,
+		title: "Servicios",
+		url: "#servicios",
+	},
+	{
+		id: 3,
+		title: "Contacto",
+		url: "#contacto",
+	},
 ];
 
-
 const Navbar = () => {
-  return (
-    <nav className="flex justify-between items-center py-2 px-8 max-h-28">
-      <Link href="#" className={styles.logo}>
-        <Image src="/logo5.svg" width={150} height={100} />
-      </Link>
-      <div className="flex gap-8">
-        {links.map((link)=> (
-          <Link href={link.url} key={link.id} className={styles.link}>{link.title}</Link>
-        ))}
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="flex justify-between items-center py-2 px-8 max-h-28">
+			<Link href="#" className="">
+				<Image src="/logo5.svg" width={150} height={100} alt="logo aria studio" />
+			</Link>
+			<div className="flex gap-8 z-10">
+				{links.map((link) => (
+					<Link href={link.url} key={link.id} className={styles.link}>
+						{link.title}
+					</Link>
+				))}
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
